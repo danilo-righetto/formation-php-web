47e90a83e3985845395c499b38a7a26e07b3afbc
Danilo Righetto <drighetto@fcl.com.br>
2023-07-03 23:07:14 -0300

    Dealing with methods

--- php-reflection/reflection.php
+++ php-reflection/reflection.php
@@ -6,12 +6,12 @@ use Alura\Reflection\ClasseExemplo;
 require_once __DIR__ . '/vendor/autoload.php';
 
 $objeto = new ClasseExemplo();
-var_dump($objeto);
+// var_dump($objeto);
 
 /* Trabalhando com uma Reflection API */
 $reflectionClass = new ReflectionClass(ClasseExemplo::class);
 
-echo $reflectionClass;
+// echo $reflectionClass;
 
 $objetoClasseExemplo = $reflectionClass->newInstance();
 $objetoClasseExemploSemConstrutor = $reflectionClass->newInstanceWithoutConstructor();
@@ -26,4 +26,20 @@ $modifiersNames = Reflection::getModifierNames($modifiers);
 
 /* Obtendos os métodos públicos e protegidos de uma classe */
 $methodsClasseExemplo = $reflectionClass->getMethods(ReflectionMethod::IS_PUBLIC | ReflectionMethod::IS_PROTECTED);
-var_dump($methodsClasseExemplo);
+// var_dump($methodsClasseExemplo);
+
+/* Executando um método com Reflection */
+$reflectionMethod = $reflectionClass->getMethod('metodoPublico');
+// var_dump($reflectionMethod->invoke($objetoClasseExemplo));
+
+/* Verificando se um método precisa de parâmetros */
+var_dump($reflectionMethod->getNumberOfParameters());
+var_dump($reflectionMethod->getNumberOfRequiredParameters());
+var_dump($reflectionMethod->getParameters());
+
+/* Filtrando os parâmetros */
+$parameters = array_filter(
+  $reflectionMethod->getParameters(), 
+  fn (ReflectionParameter $parameter) => !$parameter->isOptional()
+);
+var_dump($parameters);
