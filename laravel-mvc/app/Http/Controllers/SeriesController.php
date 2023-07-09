722fc90f9a5125452adc905592319d296cf632fa
Danilo Righetto <drighetto@fcl.com.br>
2023-07-08 21:54:21 -0300

    List Series Controller

---
+++ laravel-mvc/app/Http/Controllers/SeriesController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SeriesController extends Controller
+{
+    public function listarSeries()
+    {
+        $series = [
+            'Punisher',
+            'Lost',
+            'Sabrina'
+        ];
+
+        $html = '<ul>';
+        foreach ($series as $serie) {
+            $html .= "<li>$serie</li>";
+        }
+        $html .= '</ul>';
+
+        echo $html;
+    }
+}
