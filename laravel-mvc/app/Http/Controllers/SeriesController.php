8c6d7b46d3f980b5f31b37598b5f8c889a7c9f39
Danilo Righetto <drighetto@fcl.com.br>
2023-07-10 19:58:01 -0300

    Laravel ORM

--- laravel-mvc/app/Http/Controllers/SeriesController.php
+++ laravel-mvc/app/Http/Controllers/SeriesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Serie;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -9,21 +10,16 @@ class SeriesController extends Controller
 {
     public function index()
     {
-        $series = [
-            'Punisher',
-            'Lost',
-            'Sabrina'
-        ];
+        // $series = [
+        //     'Punisher',
+        //     'Lost',
+        //     'Sabrina'
+        // ];
 
-        $series = DB::select('SELECT nome FROM series;');
+        // $series = DB::select('SELECT nome FROM series;');
+        $series = Serie::query()->orderBy('nome')->get();
 
-        $html = '<ul>';
-        foreach ($series as $serie) {
-            $html .= "<li>$serie</li>";
-        }
-        $html .= '</ul>';
-
-        echo $html;
+        return view('series.index')->with('series', $series);
     }
 
     public function create()
@@ -36,10 +32,13 @@ class SeriesController extends Controller
         $nomeSerie = $request->input('nome');
 
         /* Modo errado de realizar essa inserção */
-        if(DB::insert('INSERT INTO series (nome) VALUES (?)', [$nomeSerie])) {
-            return "OK";
-        } else {
-            return "Deu erro";
-        }
+        // DB::insert('INSERT INTO series (nome) VALUES (?)', [$nomeSerie]);
+        // return redirect('/series');
+
+        /* Modo correto de realizar essa inserção */
+        $serie = new Serie();
+        $serie->nome = $nomeSerie;
+        $serie->save();
+        return redirect('/series');
     }
 }
