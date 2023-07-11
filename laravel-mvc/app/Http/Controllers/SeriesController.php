68adb4ac6af00802905ac47ae577f1d3cf4b9c09
Danilo Righetto <drighetto@fcl.com.br>
2023-07-11 11:57:44 -0300

    Named Routes - Laravel

--- laravel-mvc/app/Http/Controllers/SeriesController.php
+++ laravel-mvc/app/Http/Controllers/SeriesController.php
@@ -8,37 +8,49 @@ use Illuminate\Support\Facades\DB;
 
 class SeriesController extends Controller
 {
-    public function index()
-    {
-        // $series = [
-        //     'Punisher',
-        //     'Lost',
-        //     'Sabrina'
-        // ];
+  public function index()
+  {
+    // $series = [
+    //     'Punisher',
+    //     'Lost',
+    //     'Sabrina'
+    // ];
 
-        // $series = DB::select('SELECT nome FROM series;');
-        $series = Serie::query()->orderBy('nome')->get();
+    // $series = DB::select('SELECT nome FROM series;');
+    $series = Serie::query()->orderBy('nome')->get();
 
-        return view('series.index')->with('series', $series);
-    }
+    return view('series.index')->with('series', $series);
+  }
 
-    public function create()
-    {
-        return view('series.create');
-    }
+  public function create()
+  {
+    return view('series.create');
+  }
 
-    public function store(Request $request)
-    {
-        $nomeSerie = $request->input('nome');
+  public function store(Request $request)
+  {
+    // $nomeSerie = $request->input('nome');
 
-        /* Modo errado de realizar essa inserção */
-        // DB::insert('INSERT INTO series (nome) VALUES (?)', [$nomeSerie]);
-        // return redirect('/series');
+    /* Outra forma de obter a informação do campo 'nome' da requisição é essa: */
+    // $nomeSerie = $request->nome;
 
-        /* Modo correto de realizar essa inserção */
-        $serie = new Serie();
-        $serie->nome = $nomeSerie;
-        $serie->save();
-        return redirect('/series');
-    }
+    /* Modo errado de realizar essa inserção */
+    // DB::insert('INSERT INTO series (nome) VALUES (?)', [$nomeSerie]);
+    // return redirect('/series');
+
+    /* Modo correto de realizar essa inserção */
+    // $serie = new Serie();
+    // $serie->nome = $nomeSerie;
+    // $serie->save();
+
+    /* Outra forma de registrar uma Serie no banco e mais enxuta é essa */
+    Serie::create($request->all());
+
+    /* Para trazer todos os dados de uma requisição com exceção de um ou mais
+    campos utilize:
+    $request->except(['_token']);
+    */
+
+    return to_route('series.index');
+  }
 }
