fd69056d87cb21b93b26548f3c52bfe3b2017452
Danilo Righetto <drighetto@fcl.com.br>
2023-07-13 00:09:08 -0300

    Laravel Debug

--- laravel-mvc/app/Http/Controllers/SeriesController.php
+++ laravel-mvc/app/Http/Controllers/SeriesController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Serie;
+use App\Models\Series;
 use Illuminate\Http\Request;
 use App\Http\Requests\SeriesFormRequest;
 use Illuminate\Support\Facades\DB;
@@ -18,7 +18,7 @@ class SeriesController extends Controller
     // ];
 
     // $series = DB::select('SELECT nome FROM series;');
-    $series = Serie::query()->orderBy('nome')->get();
+    $series = Series::all();
     $mensagemSucesso = $request->session()->get('mensagem.sucesso');
     return view('series.index')->with('series', $series)->with('mensagemSucesso', $mensagemSucesso);
   }
@@ -40,12 +40,23 @@ class SeriesController extends Controller
     // return redirect('/series');
 
     /* Modo correto de realizar essa inserção */
-    // $serie = new Serie();
+    // $serie = new Series();
     // $serie->nome = $nomeSerie;
     // $serie->save();
 
-    /* Outra forma de registrar uma Serie no banco e mais enxuta é essa */
-    $serie = Serie::create($request->all());
+    /* Outra forma de registrar uma Series no banco e mais enxuta é essa */
+    $serie = Series::create($request->all());
+    for ($i = 1; $i <= $request->seasonsQty; $i++) {
+      $season = $serie->seasons()->create([
+        'number' => $i
+      ]);
+
+      for ($j = 1; $j <= $request->episodesPerSeason; $j++) {
+        $season->episodes()->create([
+          'number' => $j
+        ]);
+      }
+    }
 
     /* Para trazer todos os dados de uma requisição com exceção de um ou mais
     campos utilize:
@@ -55,19 +66,19 @@ class SeriesController extends Controller
     return to_route('series.index')->with('mensagem.sucesso', "Série '{$serie->nome}' adicionada com sucesso");
   }
 
-  public function destroy(Serie $series, Request $request)
+  public function destroy(Series $series, Request $request)
   {
     $series->delete();
-    // Serie::destroy($request->series);
+    // Series::destroy($request->series);
     return to_route('series.index')->with('mensagem.sucesso', "Série '{$series->nome}' removida com sucesso");
   }
 
-  public function edit(Serie $series)
+  public function edit(Series $series)
   {
     return view('series.edit')->with('series', $series);
   }
 
-  public function update(Serie $series, SeriesFormRequest $request)
+  public function update(Series $series, SeriesFormRequest $request)
   {
     $series->nome = $request->nome;
     $series->save();
--- laravel-mvc/resources/views/series/create.blade.php
+++ laravel-mvc/resources/views/series/create.blade.php
@@ -1,3 +1,29 @@
 <x-layout title="Nova Série">
-    <x-series.form :action="route('series.store')" :btname="Adicionar" :nome="old('nome')" :update="false" />
+    <form action="{{ route('series.store') }}" method="post">
+        @csrf
+
+        <div class="row mb-3">
+            <div class="col-8">
+                <div class="mb-3">
+                    <label for="nome" class="form-label">Nome:</label>
+                    <input type="text" autofocus id="nome" name="nome" class="form-control" value="{{ old('nome') }}">
+                </div>
+            </div>
+            <div class="col-2">
+                <div class="mb-3">
+                    <label for="seasonsQty" class="form-label">Nº Temporadas:</label>
+                    <input type="text" id="seasonsQty" name="seasonsQty" class="form-control" value="{{ old('seasonsQty') }}">
+                </div>
+            </div>
+            <div class="col-2">
+                <div class="mb-3">
+                    <label for="episodesPerSeason" class="form-label">Eps / Temporada:</label>
+                    <input type="text" id="episodesPerSeason" name="episodesPerSeason" class="form-control" value="{{ old('episodesPerSeason') }}">
+                </div>
+            </div>
+        </div>
+
+        <button type="submit" class="btn btn-primary">{{$btname}}</button>
+    </form>
+
 </x-layout>
