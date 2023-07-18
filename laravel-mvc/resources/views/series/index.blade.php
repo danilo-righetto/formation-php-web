c8154bf61d6701e754bb6aa89752cddc50b5564a
Danilo Righetto <drighetto@fcl.com.br>
2023-07-18 17:09:53 -0300

    Adjusting application buttons

--- laravel-mvc/resources/views/series/index.blade.php
+++ laravel-mvc/resources/views/series/index.blade.php
@@ -1,10 +1,15 @@
 <x-layout title="Séries" :mensagem-sucesso="$mensagemSucesso">
+    @auth
     <a href="{{ route('series.create') }}" class="btn btn-dark mb-2">Adicionar</a>
+    @endauth
 
     <ul class="list-group item d-flex justify-content-between align-items-center">
         @foreach ($series as $serie)
         <li class="list-group-item">
-            <a href="{{ route('seasons.index', $serie->id) }}" target="_blank" rel="noopener noreferrer">{{ $serie->nome }}</a>
+            @auth<a href="{{ route('seasons.index', $serie->id) }}" target="_blank" rel="noopener noreferrer">@endauth
+                {{ $serie->nome }}
+                @auth</a>@endauth
+            @auth
             <span class="d-flex">
                 <a href="{{ route('series.edit', $serie->id) }}" class="btn btn-primary btn-sm">E</a>
                 <form action="{{route('serie.destroy', $serie->id)}}" method="post" class="ms-2">
@@ -12,6 +17,7 @@
                     <button class="btn btn-danger btn-sm">X</button>
                 </form>
             </span>
+            @endauth
         </li>
         @endforeach
     </ul>
