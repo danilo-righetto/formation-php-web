0a1c8b3872b6fd81fc7ed7ed2f1ddd4d26780b79
Danilo Righetto <drighetto@fcl.com.br>
2023-07-18 16:54:05 -0300

    Logout

--- laravel-mvc/routes/web.php
+++ laravel-mvc/routes/web.php
@@ -38,6 +38,7 @@ Route::post('/series/{series}/episodes', [EpisodesController::class, 'update'])-
 
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'store'])->name('signin');
+Route::post('/logout', [LoginController::class, 'destroy'])->name('logout');
 
 Route::get('/register', [UsersController::class, 'create'])->name('users.create');
 Route::post('/register', [UsersController::class, 'store'])->name('users.store');
