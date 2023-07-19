aac9faaedc3321a6ae4490ee35affedcd4fb6050
Danilo Righetto <drighetto@fcl.com.br>
2023-07-19 14:02:10 -0300

    user verification

--- laravel-mvc/routes/web.php
+++ laravel-mvc/routes/web.php
@@ -1,12 +1,6 @@
 <?php
 
-use App\Http\Controllers\EpisodesController;
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\SeasonsController;
-use App\Http\Controllers\SeriesController;
-use App\Http\Controllers\UsersController;
-use App\Http\Middleware\Autenticador;
-use Illuminate\Http\Request;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,35 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-
-Route::get('/ola', function () {
-    echo 'Olá, Mundo!';
+Route::get('/', function () {
+    return view('welcome');
 });
 
-Route::resource('/series', SeriesController::class)->except(['show']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('autenticador')->group(function () {
-    Route::get('/', function () {
-        return redirect('/series');
-    });
-
-    Route::get('/series/{series}/seasons', [SeasonsController::class, 'index'])->name('seasons.index');
-
-    Route::get('/series/{series}/episodes', [EpisodesController::class, 'index'])->name('episodes.index');
-
-    Route::post('/series/{series}/episodes', [EpisodesController::class, 'update'])->name('episodes.update');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/login', [LoginController::class, 'index'])->name('login');
-Route::post('/login', [LoginController::class, 'store'])->name('signin');
-Route::post('/logout', [LoginController::class, 'destroy'])->name('logout');
-
-Route::get('/register', [UsersController::class, 'create'])->name('users.create');
-Route::post('/register', [UsersController::class, 'store'])->name('users.store');
-
-// Route::controller(SeriesController::class)->group(function () {
-//   Route::get('/series', 'index')->name('series.index');
-//   Route::get('/series/criar', 'create')->name('series.create');
-//   Route::post('/series/salvar', 'store')->name('series.store');
-// });
+require __DIR__.'/auth.php';
